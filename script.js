769702e0a109d6a441f678b52bb1e10de4675fbc
Adii verma <adiiverma40@gmail.com>
2024-04-20 02:14:25 +0530

    finnalizing javascrip

--- script.js
+++ script.js
@@ -6,9 +6,18 @@ let restart = document.querySelector("#restart");
 let text = document.querySelector("#myText");
 let inputNumber = document.querySelector("#inputNumber");
 let opponent = document.querySelector("#opponent");
+let gameScreen = document.querySelector(".gameScreen");
+let gameStation = document.querySelector("#gameStation");
+let main = document.querySelector("main");
+
+// created DOM elements
+let winScreen = document.createElement("div");
 
 // variables declaration
+
+
 let winTry = 0;
+let number;
 
 // function declaration
 function randomNumber(){
@@ -33,28 +42,80 @@ function guessed(x){
     gussedNumber.innerText = `${x}` 
 }
 
+function restartGame(){
+    console.log("restart")  
+    number = randomNumber();
+    opponent.style.fontSize = "16px";
+    gameScreen.style.height = "400px";
+    text.innerText = "";
+    gussedNumber.innerText = "";
+    inputNumber.value = "";
+    winTry = 0; 
+    gameStation.style.display = "flex";
+    winScreen.style.display = "none";
+
+}
+
 function playerWon(){
+    gameStation.style.display = "none";
+    
+    let winGameScreen = document.createElement("div");
+    winScreen.classList.add("gameScreen");
+    winGameScreen.classList.add("game");
+    main.appendChild(winScreen);
+    winScreen.appendChild(winGameScreen);
+    let winText = document.createElement("h3");
+    winText.innerText = "you win!";
+    winGameScreen.appendChild(winText);
+    let br = document.createElement("br");
+    winGameScreen.appendChild(br);
+    let winImage = document.createElement("img");
+    winImage.src = "images/alejandro-perez-pbSjZllFJWg-unsplash.jpg";
+    winImage.style.width = "200px";
+    winImage.style.height = "200px";
+    winImage.style.borderRadius = "10px";
+    winImage.style.boxShadow = "10px 10px 10px rgba(0,0,0,0.2)";
+    winGameScreen.appendChild(winImage);
+    let restartBtn = document.createElement("button");
+    restartBtn.classList.add("button-74")
+    restartBtn.innerText = "Restart";
+    winGameScreen.appendChild(restartBtn);
+    restartBtn.addEventListener("click", restartGame);
+
 
 
 
 }
 
+function youLose(){
+    alert("you lose");
+}
+
 function opponentFn(){
-    opponent.style.fontSize += "10px"
-    console.log(opponent.style.fontSize)
+    let currentsize = parseInt(window.getComputedStyle(opponent).fontSize);
+    let currentHeight = parseInt(window.getComputedStyle(gameScreen).height);
+    console.log(currentsize)
+    if(currentsize < 25){
+    gameScreen.style.height = (currentHeight + 5) + "px";
+    opponent.style.fontSize = (currentsize + 1) + "px";
+}
+else if(currentSize = 25){
+    youLose();
+}
+
 }
 
 
 
 // event listener declaration
-let number = randomNumber();
+number = randomNumber();
 form.addEventListener("submit", (event) =>{
     event.preventDefault();
     console.log(number);
     winTry++;
     let guess = parseInt(inputNumber.value);
     if(number === guess){
-        alert(`you win in ${winTry} moves`);
+        playerWon();
     }
     else if(guess > number && guess <= 100){
        greaterThan();
@@ -74,3 +135,4 @@ form.addEventListener("submit", (event) =>{
 
 
 })
+restart.addEventListener("click", restartGame);
